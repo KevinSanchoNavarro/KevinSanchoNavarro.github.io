71decc74fc82767ccb100b665ee618a0f62c9bee
usrApoyoSIS17 <usrApoyoSIS17@PC-STICVIL04>
2020-12-04 15:29:53 -0500

    ---

--- static/assets/js/Productos.js
+++ static/assets/js/Productos.js
@@ -20,6 +20,26 @@
 
 })();
 
+(function($) {
+    $.fn.inputFilter = function(inputFilter) {
+      return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+          this.value = "";
+        }
+      });
+    };
+}(jQuery));
+
+$(".seleccionar-productos__contador .seleccionar-productos__quantity").inputFilter(function(value) {
+    return /^-?\d*$/.test(value); 
+});
 
 function SeleccionarProductos() {
     $("#divLoading").show();
